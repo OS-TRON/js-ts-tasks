9a39a4faf4e92617c66ab529a2cb4a4f9b32cc56
Leskort <starlaider4@gmail.com>
2025-01-27 16:52:54 +0300

    feat: commit all

--- src/all.js
+++ src/all.js
@@ -3,6 +3,21 @@
  * @param {Array<Promise>} promisesArray
  * @returns Promise
  */
+const { error } = require('lint-staged/lib/figures');
 module.exports.all = function all(promisesArray) {
-  throw new Error('Not implemented'); // remove me and write your code
+  const results = [];
+  let count = 0;
+  return new Promise((resolve, reject) => {
+    promisesArray.forEach(async (promisesArray, index) => {
+      try {
+        results[index] = await promisesArray;
+        count += 1;
+        if (count === promisesArray.length) {
+          resolve(results);
+        }
+      } catch (error) {
+        reject(error);
+      }
+    });
+  });
 };
