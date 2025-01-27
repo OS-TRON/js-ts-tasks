d427accaaf55db82a91ea5bf69287d16c446f457
Leskort <starlaider4@gmail.com>
2025-01-27 17:01:31 +0300

    feat: commit a1l

--- src/all.js
+++ src/all.js
@@ -3,14 +3,14 @@
  * @param {Array<Promise>} promisesArray
  * @returns Promise
  */
-const { error } = require('lint-staged/lib/figures');
+
 module.exports.all = function all(promisesArray) {
   const results = [];
   let count = 0;
   return new Promise((resolve, reject) => {
-    promisesArray.forEach(async (promisesArray, index) => {
+    promisesArray.forEach(async (promisesInArray, index) => {
       try {
-        results[index] = await promisesArray;
+        results[index] = await promisesInArray;
         count += 1;
         if (count === promisesArray.length) {
           resolve(results);
