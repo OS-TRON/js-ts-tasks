60dc71e345ea543ada62df640d072fd00af0648a
OS-TRON <lehaleskortov@gmail.com>
2025-02-04 21:51:20 +0300

    Update debounce.js

--- src/debounce.js
+++ src/debounce.js
@@ -6,5 +6,13 @@
  * @return {function}
  */
 module.exports.debounce = function debounce(fn, delay) {
-  throw new Error('Not implemented'); // remove me and write your code
+   let timer;
+  return (...args) => {
+    if (!timer) fn(...args);
+    clearTimeout(timer);
+
+    timer = setTimeout(() => {
+      fn(...args);
+    }, delay);
+  };
 };
